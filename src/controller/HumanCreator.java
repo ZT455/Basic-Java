ed4909bd53c84dcb75fc48673c1bb8ec5f9e87c7
ZT455 <zhenya.tkachenko.50@gmail.com>
2024-03-25 19:25:09 +0200

    -

---
+++ src/controller/HumanCreator.java
@@ -0,0 +1,21 @@
+package controller;
+import model.Human;
+import model.Sex;
+
+public class HumanCreator {
+    public Human createHuman(String name, String surname, String patronymic, Sex sex) {
+
+        Human human = new Human();
+        human.setName(name);
+        human.setPatronymic(patronymic);
+        human.setSex(sex);
+        human.setSurname(surname);
+
+        return human;
+    }
+    public  Human oneMan(){
+
+        return createHuman("Walter","White","Jameson",Sex.MALE);
+    }
+}
+
---
+++ src/model/Group.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Group  {
+
+    private int maxNumberOfStudents;
+    List<Student> listStudents;
+
+    public List<Student> getListStudents() {
+        return listStudents;
+    }
+    public void setStudents(List<Student> listStudents) {
+        this.listStudents = listStudents;
+    }
+
+    public int getMaxNumberOfStudents() {
+        return maxNumberOfStudents;
+    }
+
+    public void setMaxNumberOfStudents(int maxNumberOfStudents) {
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Group{" +
+                "maxNumberOfStudents=" + maxNumberOfStudents +
+                ", students=" + listStudents.toString() +
+                '}';
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (!(o instanceof Group)) return false;
+
+        Group group = (Group) o;
+
+        return maxNumberOfStudents == group.maxNumberOfStudents &&
+                listStudents.equals(group.listStudents);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(maxNumberOfStudents, listStudents);
+    }
+}
